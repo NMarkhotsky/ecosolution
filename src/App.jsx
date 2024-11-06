d155df84efbd44714e66506f288bcc58a4fd8ddd
Nikolay <marhotsky@gmail.com>
2024-11-06 17:36:10 +0200

    Update :)

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,29 @@ import { GlobalStyle } from './styles';
 import { SharedLayout } from './components/SharedLayout/SharedLayout';
 import { theme } from './theme/theme';
 import { fonts } from './constants/fonts';
+import { Bars } from 'react-loader-spinner';
 
 function App() {
-  const fontsLoaded = useFonts(fonts);
+	const fontsLoaded = useFonts(fonts);
 
-  return fontsLoaded ? (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <SharedLayout />
-    </ThemeProvider>
-  ) : (
-    <div>Loading...</div>
-  );
+	return fontsLoaded ? (
+		<ThemeProvider theme={theme}>
+			<GlobalStyle />
+			<SharedLayout />
+		</ThemeProvider>
+	) : (
+		<div style={{ position: 'absolute', top: '50%', left: '50%' }}>
+			<Bars
+				height="80"
+				width="80"
+				color="#4fa94d"
+				ariaLabel="bars-loading"
+				wrapperStyle={{}}
+				wrapperClass=""
+				visible={true}
+			/>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,92 +1,76 @@
 import { useEffect, useRef, useState } from 'react';
 import { Icon } from '../../shared/Icon/Icon';
-import {
-  HeaderContainer,
-  Logo,
-  MenuWrapper,
-  ButtonMenu,
-  ButtonGetInTouch,
-} from './Header.styled';
+import { HeaderContainer, Logo, MenuWrapper, ButtonMenu, ButtonGetInTouch } from './Header.styled';
 import { BurgerMenu } from '../BurgerMenu/BurgerMenu';
 import { scrollToElement } from '../../utils';
 
 export const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isScrolling, setIsScrolling] = useState(false);
-  const scrollTimeoutRef = useRef(null);
+	const [isOpen, setIsOpen] = useState(false);
+	const [isScrolling, setIsScrolling] = useState(false);
+	const scrollTimeoutRef = useRef(null);
 
-  const handleToggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+	const handleToggleMenu = () => {
+		setIsOpen(!isOpen);
+	};
 
-  const handleScroll = () => {
-    setIsScrolling(true);
+	const handleScroll = () => {
+		setIsScrolling(true);
 
-    if (scrollTimeoutRef.current !== null) {
-      clearTimeout(scrollTimeoutRef.current);
-    }
+		if (scrollTimeoutRef.current !== null) {
+			clearTimeout(scrollTimeoutRef.current);
+		}
 
-    scrollTimeoutRef.current = setTimeout(() => {
-      setIsScrolling(false);
-    }, 250);
-  };
+		scrollTimeoutRef.current = setTimeout(() => {
+			setIsScrolling(false);
+		}, 250);
+	};
 
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
 
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-      if (scrollTimeoutRef.current !== null) {
-        clearTimeout(scrollTimeoutRef.current);
-      }
-    };
-  }, []);
-  return (
-    <HeaderContainer $isScrolling={isScrolling}>
-      <Logo href="/ecosolution/">
-        <Icon
-          iconName="icon-favicon_logo"
-          width={'31px'}
-          height={'18px'}
-          fill={'inherit'}
-        />
-        <Icon
-          iconName="icon-ecosolution"
-          width={'170px'}
-          height={'40px'}
-          fill={'inherit'}
-        />
-        <Icon
-          iconName="icon-greenergy"
-          width={'60px'}
-          height={'18px'}
-          fill={'inherit'}
-        />
-      </Logo>
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+			if (scrollTimeoutRef.current !== null) {
+				clearTimeout(scrollTimeoutRef.current);
+			}
+		};
+	}, []);
+	return (
+		<HeaderContainer $isScrolling={isScrolling}>
+			<Logo href="/ecosolution">
+				<Icon
+					iconName="icon-favicon_logo"
+					width={'31px'}
+					height={'18px'}
+					fill={'inherit'}
+				/>
+				<Icon
+					iconName="icon-ecosolution"
+					width={'170px'}
+					height={'40px'}
+					fill={'inherit'}
+				/>
+				<Icon iconName="icon-greenergy" width={'60px'} height={'18px'} fill={'inherit'} />
+			</Logo>
 
-      <MenuWrapper>
-        <ButtonMenu type="button" onClick={handleToggleMenu}>
-          <Icon
-            iconName="icon-menu"
-            width={'16px'}
-            height={'16px'}
-            stroke={'#000'}
-          />
-        </ButtonMenu>
-        {isOpen && <BurgerMenu onClose={handleToggleMenu} />}
-        <ButtonGetInTouch
-          href="#contact-us"
-          onClick={(e) => {
-            e.preventDefault();
-            scrollToElement('contact-us');
-          }}
-        >
-          Get in touch
-          <span>
-            <Icon iconName="icon-arrow-down" width={14} height={14} />
-          </span>
-        </ButtonGetInTouch>
-      </MenuWrapper>
-    </HeaderContainer>
-  );
+			<MenuWrapper>
+				<ButtonMenu type="button" onClick={handleToggleMenu}>
+					<Icon iconName="icon-menu" width={'16px'} height={'16px'} stroke={'#000'} />
+				</ButtonMenu>
+				{isOpen && <BurgerMenu onClose={handleToggleMenu} />}
+				<ButtonGetInTouch
+					href="#contact-us"
+					onClick={(e) => {
+						e.preventDefault();
+						scrollToElement('contact-us');
+					}}
+				>
+					Get in touch
+					<span>
+						<Icon iconName="icon-arrow-down" width={14} height={14} />
+					</span>
+				</ButtonGetInTouch>
+			</MenuWrapper>
+		</HeaderContainer>
+	);
 };
